f6d8d20bd8f30c1cd542d061500e2a4bfb06806c
Vanessa Ragan <azurewings@csu.fullerton.edu>
2022-10-11 19:44:22 -0700

    Update CPSC335_Project2_Alg2.cpp
    Takes an input of any size. For the words the user needs to separate the list with spaces.

--- CPSC335_Project2_Alg2.cpp
+++ CPSC335_Project2_Alg2.cpp
@@ -3,14 +3,15 @@
 #include <vector>
 using namespace std;
 
-const int SIZE = 4;
-string arrayOutput[SIZE], test;
-int orderOutput[SIZE], index = 0;
+string test;
+int index = 0;
+vector<string> arrayOutput;
+vector<int> orderOutput;
 
-void findWords (string inputArray, vector<string> names)
+void findWords (string inputArray, vector<string> names, int words)
 {
     for(int i = 0; i < inputArray.size(); i++){
-        for(int j = 0; j < SIZE; j++){
+        for(int j = 0; j < words; j++){
             // if the first and last letters of names[j] equal inputArray[i] and inputArray[i+len(names[j]-1)] respectively 
             if(names[j][0] == inputArray[i] && names[j][(size(names[j])-1)] == inputArray[(i+(size(names[j])-1))])
             {
@@ -22,9 +23,8 @@ void findWords (string inputArray, vector<string> names)
                     test += inputArray[i+k];
                 }
                 if(names[j] == test){
-                    orderOutput[index] = i;
-                    arrayOutput[index] = test;
-                    index++;
+                    orderOutput.push_back(i);
+                    arrayOutput.push_back(test);
                 }
             }
         }
@@ -33,34 +33,43 @@ void findWords (string inputArray, vector<string> names)
 
 int main()
 {
-    string longArray, temp;
-    int numWords;
+    string longArray, temp, hold;
+    int numWords = 0;
     vector<string> namesArray;
 
     cout << "Please enter a string of letters with words scattered in it: ";
-    cin >> longArray;
-    cout << "Please enter the number of words you'd like to look for: ";
-    cin >> numWords;
-    cout << "Please enter a list of words to find. Seperate each word by pressing enter: ";
-    for(int t = 0; t < numWords; t++)
+    getline(cin, longArray);
+    cout << "Please enter a list of words to find, seperated by spaces (Ex: foo foo2): ";
+    getline(cin, temp);
+    for(int t = 0; t < temp.size(); t++)
     {
-        string hold;
-        cin >> hold;
-        namesArray.push_back(hold);
+        if(temp[t] != ' ')
+        {
+            hold += temp[t];
+        }
+        else
+        {
+            namesArray.push_back(hold);
+            numWords++;
+            hold = "";
+        }
     }
+    namesArray.push_back(hold);
+    numWords++;
 
-    findWords(longArray, namesArray);
+
+    findWords(longArray, namesArray, numWords);
     
     cout << "Output_order = [";
-    for(int i = 0; i < SIZE; i++){
+    for(int i = 0; i < numWords; i++){
         cout << orderOutput[i];
-        if(i < SIZE-1)
+        if(i < numWords-1)
             cout << ", ";
     }
     cout << "]" << endl << "Output_array = [";
-    for(int i = 0; i < SIZE; i++){
+    for(int i = 0; i < numWords; i++){
         cout << "'" << arrayOutput[i] << "'";
-        if(i < SIZE-1)
+        if(i < numWords-1)
             cout << ", ";
     }   
       
