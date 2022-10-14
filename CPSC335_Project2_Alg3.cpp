5353ed6d6d082b2d2550382c692fd5f30c4487ea
Vanessa Ragan <azurewings@csu.fullerton.edu>
2022-10-14 10:03:23 -0700

    Update CPSC335_Project2_Alg3.cpp
    It can take user input, but the way it's doing it isn't ideal.

--- CPSC335_Project2_Alg3.cpp
+++ CPSC335_Project2_Alg3.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <string>
 using namespace std;
 vector<int> mergedList;
 
@@ -44,7 +43,9 @@ void mergeSort(vector<int> inputVect, vector<int> inputVect2){
 int main()
 { 
     vector<vector<int>> all_lists;
-    int numRows = 0, temp;
+    int numClasses = 0;
+    //int numRows = 0, temp;
+    //bool input = true;
     //vector<vector<int>> all_lists = {
     //    {2, 5, 9, 21},
     //    {-1, 0, 2},
@@ -64,9 +65,9 @@ int main()
     //     {5, 6, 10, 20, 24}};
     // [-4, -2, 0, 2, 4, 5, 6, 6, 7, 10, 10, 12, 14, 15, 20, 24, 25]
 
-    cout<< "Please enter the classes grades in non-decreasing order." << endl
-        << "Seperate each class with enter." << endl
-        << "When finished, please press enter again: ";
+    //cout<< "Please enter the classes grades in non-decreasing order, seperating each grade with an enter." << endl
+    //    << "Seperate each class by pressing enter without inputting a grade." << endl
+    //    << "When finished, please press enter again: ";
 
     //Doesn't work, keep getting read access violations.
     //Also doesn't work if I try and read it in like a string and turn that into an int
@@ -85,6 +86,35 @@ int main()
     //        break;
     //    numRows++;
     //}
+    cout << "Please enter the number of classes: ";
+    cin >> numClasses;
+    while(numClasses > 0)
+    {
+        int x = 0, y = 0, z = 0;
+        vector<int> hold;
+        cout << "Please input the number of grades this class has: ";
+        cin >> y;
+        while(y <= 0)
+        {
+            cout << "Please input a positive number for the number of class grades: ";
+            cin >> y;
+        }
+        cout << "Now please input the grades in non-decreasing order, seperating each grade with the enter key." << endl;    
+        while(z < y)
+        {
+            cin >> x;
+            hold.push_back(x);
+            z++;
+        }
+        all_lists.push_back(hold);
+        numClasses--;
+        //numRows++;
+    }
+    
+    
+
+    //all_lists.push_back(hold);
+
     int rowNum = 0;
     while(rowNum < size(all_lists))
     {
